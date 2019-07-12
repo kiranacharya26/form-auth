c97afb409340fa95f774ce8486e59f2cd55e0679
kiran acharya <acharya.kiran26ka@gmail.com>
2019-07-13 02:19:23 +0530

    added Logout button and javascrpit funtion to display the same

--- auth.js
+++ auth.js
@@ -1,12 +1,10 @@
 auth.onAuthStateChanged(user => {
   if (user) {
     console.log('user logged in: ', user);
-    db.collection('guides').get().then(snapshot => {
-      setupGuides(snapshot.docs);
-    });
+    
   } else {
     console.log('user logged out');
-    setupGuides([]);
+  
   }
 })
 
@@ -33,24 +31,26 @@ signupForm.addEventListener('submit', (e) => {
   });
 });
 
+// logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-  auth.signOut();
-});
+  auth.signOut()
+    
+  });
 
-// login
+
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
-  // get user info
+  
   const email = loginForm['login-email'].value;
   const password = loginForm['login-password'].value;
 
-  // log the user in
+  
   auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    // close the signup modal & reset form
+    
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
     loginForm.reset();
